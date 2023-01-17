ef84ebfde7031472c930eef90f1145fc0d367eef
Arvindh <arvindh91@gmail.com>
2023-01-17 16:16:40 +0530

    update: certs postgres migration
    Signed-off-by: Arvindh <arvindh91@gmail.com>

--- certs/postgres/init.go
+++ certs/postgres/init.go
@@ -61,23 +61,47 @@ func migrateDB(db *sqlx.DB) error {
 				Id: "certs_1",
 				Up: []string{
 					`CREATE TABLE IF NOT EXISTS certs (
-						id           UUID NOT NULL,
-						name 		 VARCHAR(254) NOT NULL,
-						owner_id     VARCHAR(254) NOT NULL,
-						thing_id     UUID NOT NULL,
-						serial       VARCHAR(254) NOT NULL,
-						private_key  TEXT NOT NULL,
-						certificate  TEXT NOT NULL,
-						ca_chain     TEXT NOT NULL,
-						issuing_ca   TEXT NOT NULL,
-						ttl          TIMESTAMPTZ NOT NULL,
+						thing_id     TEXT NOT NULL,
+						owner_id     TEXT NOT NULL,
 						expire       TIMESTAMPTZ NOT NULL,
-						revocation   TIMESTAMPTZ NULL,
-						PRIMARY KEY  (id, thing_id, owner_id)
+						serial       TEXT NOT NULL,
+						PRIMARY KEY  (thing_id, owner_id, serial)
 					);`,
 				},
 				Down: []string{
 					"DROP TABLE IF EXISTS certs;",
+				},
+			},
+
+			{
+				Id: "certs_2",
+				Up: []string{
+					`					
+					ALTER TABLE certs DROP CONSTRAINT certs_pkey;
+					ALTER TABLE certs ADD COLUMN id UUID NOT NULL;
+					ALTER TABLE certs ADD COLUMN name VARCHAR(256) NULL;
+					ALTER TABLE certs ADD COLUMN certificate TEXT NOT NULL;
+					ALTER TABLE certs ADD COLUMN private_key TEXT NOT NULL;
+					ALTER TABLE certs ADD COLUMN ca_chain TEXT NOT NULL;
+					ALTER TABLE certs ADD COLUMN issuing_ca TEXT NOT NULL;
+					ALTER TABLE certs ADD COLUMN ttl VARCHAR(256) NOT NULL;
+					ALTER TABLE certs ADD COLUMN revocation TIMESTAMPTZ NULL;
+					ALTER TABLE certs ADD PRIMARY KEY (id, thing_id, owner_id);
+					`,
+				},
+				Down: []string{
+					`
+					ALTER TABLE certs DROP CONSTRAINT certs_pkey;
+					ALTER TABLE certs DROP COLUMN id data_type;
+					ALTER TABLE certs DROP COLUMN name data_type;
+					ALTER TABLE certs DROP COLUMN certificate data_type;
+					ALTER TABLE certs DROP COLUMN private_key data_type;
+					ALTER TABLE certs DROP COLUMN ca_chain data_type;
+					ALTER TABLE certs DROP COLUMN issuing_ca data_type;
+					ALTER TABLE certs DROP COLUMN ttl data_type;
+					ALTER TABLE certs DROP COLUMN revocation data_type;
+					ALTER TABLE certs ADD PRIMARY KEY (thing_id, owner_id, serial);
+					`,
 				},
 			},
 		},
