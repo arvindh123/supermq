2915f52aa154a33652d294fe4c568b60e1e704b8
b1ackd0t <blackd0t@protonmail.com>
2023-05-16 18:09:28 +0300

    NOISSUE - Fix Bugs On Syncing With Docs (#1759)
    * Move Things and Users to Clients
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Sync Users API Documentation with Implementation
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Fix Update Secret SDK
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Make Thing Certs using Thing Secret
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Fix Update User
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Add Id as Parameter During Update
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Log OK
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Remove Level
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Enable Groups By Default
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Remove Hyphen For Initializing Metrics
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Remove Hyphen For Initializing Metrics
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Add MF_PROVISION_CERTS_RSA_BITS to env
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Fix mTLS on nginx config
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Fix When ID is Not Empty
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Update api/openapi/users.yml
    
    Co-authored-by: Sammy Kerata Oina <44265300+SammyOina@users.noreply.github.com>
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Update cmd/bootstrap/main.go
    
    Co-authored-by: Sammy Kerata Oina <44265300+SammyOina@users.noreply.github.com>
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Add description For Client and Group Status
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Add Struct Tags
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    ---------
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    Co-authored-by: Sammy Kerata Oina <44265300+SammyOina@users.noreply.github.com>

--- users/policies/api/http/responses.go
+++ users/policies/api/http/responses.go
@@ -17,10 +17,9 @@ var (
 )
 
 type pageRes struct {
-	Limit  uint64 `json:"limit,omitempty"`
-	Offset uint64 `json:"offset,omitempty"`
+	Limit  uint64 `json:"limit"`
+	Offset uint64 `json:"offset"`
 	Total  uint64 `json:"total"`
-	Level  uint64 `json:"level"`
 }
 
 type authorizeRes struct {
