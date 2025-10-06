1c3d277ac324202d5cfd12e34ad13380033d69d3
Arvindh <arvindh91@gmail.com>
2025-10-06 16:55:29 +0530

    add new error and new error types
    Signed-off-by: Arvindh <arvindh91@gmail.com>

--- pkg/errors/types.go
+++ pkg/errors/types.go
@@ -25,10 +25,13 @@ var (
 	ErrRollbackTx = errors.New("failed to rollback transaction")
 
 	// ErrAuthentication indicates failure occurred while authenticating the entity.
-	ErrAuthentication = errors.New("failed to perform authentication over the entity")
+	ErrAuthentication = NewAuthNError("failed to perform authentication over the entity")
 
 	// ErrAuthorization indicates failure occurred while authorizing the entity.
-	ErrAuthorization = errors.New("failed to perform authorization over the entity")
+	ErrAuthorization = NewAuthZError("failed to perform authorization over the entity")
+
+	// ErrDomainAuthorization indicates failure occurred while authorizing the domain.
+	ErrDomainAuthorization = NewAuthZError("failed to perform authorization over the domain")
 
 	// ErrMissingDomainMember indicates member is not part of a domain.
 	ErrMissingDomainMember = errors.New("member id is not member of domain")
